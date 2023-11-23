4846b1be90f85e195ff2f4cb08d1d5f813d71958
S-Romeaux <sal.4845@gmail.com>
2023-11-22 21:06:14 -0800

    Done Day 8

--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,17 @@
+import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event'
+import App from './App';
+
+beforeEach(() => {
+    // sets everything back to initial state before each test
+    fetch.resetMocks();
+  })
 
 
 
+test("receives GitHub name from GitHub REST API using jest fetch mock", async () => {
+    fetch.mockResponseOnce(JSON.stringify({name: 'coder'}))
+    render(<App />)
+    const gitHubName = await waitFor(() => screen.getByRole('heading', { level: 2 }))
+    expect(gitHubName).toHaveTextContent('coder')
+  })
